5a9e7900de77bfce991a1564bd80068cba217278
charleswright8877 <charleswright8877@gmail.com>
2026-04-02 23:00:45 +0100

    remove public access bucket  s3

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-aws cloudformation create-stack --stack-name pipeline-taskcat --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://cf_temp.yml --parameters ParameterKey=GitHubUser,ParameterValue=charleswright8877 ParameterKey=GitHubRepo,ParameterValue=taskcat
+aws cloudformation create-stack --stack-name pipeline-taskcat --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://cf_temp.yml --parameters ParameterKey=GitHubUser,ParameterValue=charleswright8877 ParameterKey=GitHubRepo,ParameterValue=mytaskcat2
 
 # Sample pipeline https://gist.github.com/kevinkarwaski/00aa01826d88650ae5c0da54ae93f258
